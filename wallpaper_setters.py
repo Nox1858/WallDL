697cf4c70ab603da4045a275df9f185bed6b4288
Nox1858 <nox308421@gmail.com>
2026-04-16 13:29:08 +0200

    Forgot to add wallpaper_setters.py

---
+++ wallpaper_setters.py
@@ -0,0 +1,59 @@
+import subprocess
+from pathlib import Path
+from datetime import datetime
+from filehandler import getData, setData
+
+def add_occurance(img_id, ctx: AppContext):
+    Path("latest.txt").write_text(img_id)
+    with open("history.txt", "a") as f:
+        f.write(f"{img_id}; {datetime.now()}\n")
+
+    imgdata = getData(img_id, ctx.cache_dir)
+    imgdata["occurances"] = imgdata.get("occurances", 0) + 1
+    setData(img_id, imgdata, ctx.cache_dir)
+
+def setWallpaper_Android(wallpaper_path, img_id, ctx: AppContext):
+    subprocess.Popen(f'am start -a android.intent.action.ATTACH_DATA -t "image/*" -d file:{wallpaper_path}', shell=True)
+    add_occurance(img_id, ctx)
+
+
+def setWallpaper_MacOS(wallpaper_path, img_id, ctx: AppContext):
+    script = f"""
+    osascript -e 'tell application "Finder" to set desktop picture to POSIX file "{wallpaper_path}"'
+    """
+    os.system(script)
+    add_occurance(img_id, ctx)
+
+
+def setWallpaper_Plasma(wallpaper_path, img_id, ctx: AppContext):
+    subprocess.run(
+        ["plasma-apply-wallpaperimage", str(wallpaper_path)],
+        check=False,
+    )
+
+    desktop_link_path = Path(ctx.env.get("DESKTOP_PATH")) / "wallpaperURL.desktop"
+    desktop_link_path.write_text(
+        (
+            "[Desktop Entry]\n"
+            f"Icon={ctx.cache_dir / 'gelbooru-logo.svg'}\n"
+            "Name=wallpaperURL\n"
+            "Type=Link\n"
+            f"URL[$e]=https://gelbooru.com/index.php?page=post&s=view&id={img_id}"
+        )
+    )
+
+    add_occurance(img_id, ctx)
+
+def setWallpaper(image: str, ctx: AppContext) -> None:
+    wallpaper_path = ctx.wallpaper_dir / image
+    img_id = Path(image).stem
+    DESKTOP_MANAGER = ctx.env.get("DESKTOP_MANAGER")
+    match DESKTOP_MANAGER:
+        case "Plasma":
+            setWallpaper_Plasma(wallpaper_path, img_id, ctx)
+        case "MacOS":
+            setWallpaper_MacOS(wallpaper_path, img_id, ctx)
+        case "Android":
+            setWallpaper_Android(wallpaper_path, img_id, ctx)
+
+
